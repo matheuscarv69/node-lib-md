55dbbd4027ccb20dcffc1411c7aabe1f1fd12903
matheuscarv69 <matheus9126@gmail.com9>
2023-03-05 17:21:10 -0400

    :sparkles: feat: creating file or directory verification in Cli.js

--- src/Cli.js
+++ src/Cli.js
@@ -1,10 +1,41 @@
+import chalk from 'chalk'
+import fs from 'fs'
 import readAndGetLinksOfFile from "./FileLinksHelper.js"
 
-const consoleArgsArray = process.argv 
+const consoleArguments = process.argv
 
-// This second position of consoleArgsArray contains our filePath of text file md
-const filePath = consoleArgsArray[2]
+// This second position of consoleArguments contains our path of text or directory
+const path = consoleArguments[2]
 
-const linksResult = await readAndGetLinksOfFile(filePath)
+processText(path)
 
-console.log(linksResult)
+
+async function processText(path) {
+
+  if (fs.statSync(path).isFile()) {
+
+    const listLinks = await readAndGetLinksOfFile(path)
+    printListLinks(listLinks)
+
+  }
+
+  if (fs.statSync(path).isDirectory()) {
+
+    const files = await fs.promises.readdir(path)
+
+    files.forEach(async (fileName) => {
+
+      const listLinks = await readAndGetLinksOfFile(`${path}/${fileName}`)
+      printListLinks(listLinks)
+
+    })
+
+  }
+
+}
+
+function printListLinks(links) {
+
+  console.log(chalk.yellow("Listing Links: "), links);
+
+}
